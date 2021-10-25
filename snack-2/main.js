a75a54854ba6d8554db689c7cddc7c4247842aeb
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-25 12:27:10 +0200

    Complete Request
    I create an array of objects with the following properties indicated.

---
+++ snack-2/main.js
@@ -0,0 +1,24 @@
+/* Descrizione:
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
+Nome sarà l’unica proprietà da compilare, mentre le altre saranno tutte settate a 0. (modificato)  */
+let pointsMade = 0
+let pointSuffered = 0
+
+const teamSoccer = [{
+        name: 'Roma',
+        pointsMade,
+        pointSuffered
+    },
+    {
+        name: 'Lazio',
+        pointsMade,
+        pointSuffered
+    },
+    {
+        name: 'Juventus',
+        pointsMade,
+        pointSuffered
+    }
+]
+console.log(teamSoccer);
