a9c00e57d2e2b2c9bf4b4a4d03703b58e95a3e9a
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-25 15:04:13 +0200

    Add request 3

--- snack-2/main.js
+++ snack-2/main.js
@@ -2,7 +2,7 @@
 Creare un array di oggetti di squadre di calcio.
 Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
 Nome sarà l’unica proprietà da compilare, mentre le altre saranno tutte settate a 0. (modificato)  */
-
+const result = document.querySelector('.result')
 const teamSoccer = [{
             name: 'Roma',
             pointsMade: 0,
@@ -21,27 +21,32 @@ const teamSoccer = [{
     ]
     //per aggiungere i punti fatti
 for (let index = 0; index < teamSoccer.length; index++) {
-    console.log(teamSoccer[index].pointsMade);
     if (teamSoccer[index].pointsMade == 0) {
         num = generateNumberRandom()
         teamSoccer[index].pointsMade = num
-        console.log(teamSoccer[index].pointsMade);
+
     }
 }
 //per aggiungere i punti subiti
 for (let index = 0; index < teamSoccer.length; index++) {
-    console.log(teamSoccer[index].pointSuffered);
     if (teamSoccer[index].pointSuffered == 0) {
         num = generateNumberRandom()
         teamSoccer[index].pointSuffered = num
-        console.log(teamSoccer[index].pointSuffered);
     }
 }
 
+for (let index = 0; index < teamSoccer.length; index++) {
+    element = `<div class="px-4">Nome Squadra: ${teamSoccer[index].name},<br>
+                punti subiti:${teamSoccer[index].pointsMade},<br>
+                punti fatti:${teamSoccer[index].pointSuffered} </div>`
+    result.innerHTML += element
+}
 
 function generateNumberRandom() {
     numRandom = Math.floor((Math.random() * 100) + 1);
     return numRandom
 
 }
+
+
 console.log(teamSoccer);
---
+++ snack-3/main.js
@@ -0,0 +1,2 @@
+/* Descrizione
+Usando la destrutturazione ,creare un nuovo array i cui elementi contengono solo nomi e falli subiti e stampare tutto in console. */
